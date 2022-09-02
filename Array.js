9acebfbd3e1b70a8acb81eb128034d3f8246fd62
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 21:17:32 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,21 @@
+// var x ="rakhi";
+// x = -34.57;
+// x =true;
+// x=["html","c.ss"];
+// console.log(x);
+// console.log(typeof x);
+
+var firstName ="rakhi";
+var lastName ="kashyap";
+
+var age =34;
+
+var isram =true;
+var salary =null;
+var height;
+
+age ="thrity three"
+console.log(typeof age)
+
+console.log(firstName,age,height,salary,isram);
+  

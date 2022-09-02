ee2cfd956ee964e531d1855c0d73428ee9994ab7
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 21:20:39 +0530

    Add files via upload

---
+++ sum .js
@@ -0,0 +1,13 @@
+var age =15;
+
+switch(true){
+    case(age >=15 && age <=20):
+        console.log("you are Eligible");
+    break;
+    case (age >= 21 && age <=30):
+        console.log("you are not eligible");
+    
+    break;
+    default:
+        console.log("enter the valid age");
+}

f5b1fcee3a0c74c7cbac56f888ec0eee8dd1b2d9
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 21:17:57 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,30 @@
+var day =3;
+
+switch (day){
+    case 0:case 1: case 2:
+        console.log("today is monday");
+    break;
+    case 1:
+        console.log("today is tuesday");
+    break;
+    case 2:
+        console.log("today is wednesday");
+    break;
+    case 3:
+        console.log("today is thursday");
+    break;
+    case 4:
+        console.log("today is friday");
+    break;
+    case 5:
+        console.log("today is saturady");
+    break;
+    case 6:
+        console.log("sunday");
+    break;
+    
+    default:
+        console.log("Entre the valid week day");
+
+
+}

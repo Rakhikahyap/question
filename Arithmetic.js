7576ddc9f48a5824887676505d146a31287ded0b
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 21:17:14 +0530

    Add files via upload

---
+++ Arithmetic.js
@@ -0,0 +1,35 @@
+var a= 2;
+var b= 3;
+var c= a+b ;
+console.log(c);
+
+Asingment
+
+var a=10;
+var b=5;
+a+=b;
+console.log(a);
+
+camparison
+
+var x =15;
+var y =25;
+x>y;
+
+var x=20;
+var y=30;
+
+console.log(x==y);
+
+var a=19;
+var b="10";
+console.log(a===b);
+
+var a=19;
+var b="10";
+console.log(2 !=='3');
+
+var a=19;
+var b="10";
+console.log(19>=19);
+

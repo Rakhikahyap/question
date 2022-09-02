743552a97dba4674f53794de1ac98efb43392237
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 21:18:30 +0530

    Add files via upload

---
+++ per.js
@@ -0,0 +1,20 @@
+var per =120;
+
+if(per >=80 && per <=100){
+    console.log("you are in merit");
+
+}else if(per >=60 && per <80){
+    console.log("you are in 1st div");
+
+}else if(per >=45 && per <60){
+    console.log("you are in 2nd divi");
+
+}else if(per >=33 && per <45){
+    console.log("you are in 3d divi");
+
+}else if(per<33){
+    console.log("you are fail");
+
+}else{
+    console.log("please enter vaild per");
+}
